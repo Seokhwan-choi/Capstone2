8b0f7f32bd1b87b179868482d680909ca198d524
seokhwan_choi <choiseok35@nate.com>
2018-06-11 03:10:26 +0900

    인벤토리 제작
    인벤토리창 on/off 가능
    
    아이템 추가 스크립트 상으로 구현되있는데 화면에 전혀 추가되지 않음...
    
    막혀서 멘붕옮

---
+++ Test Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<GameObject> Allslot;
+
+    private string name;
+    GameObject slot;
+
+    public GameObject a, b, c;
+
+    private Items item;
+    private float EmptySlot;
+    
+    void Start()
+    {
+        slot = transform.Find("Imange").gameObject;
+        Allslot.Add(slot);
+        for (int i = 1; i < 8; i++)
+        {
+            name = "Image(" + i.ToString() + ")";
+            slot = transform.Find(name).gameObject;
+            Allslot.Add(slot);
+        }
+        EmptySlot = Allslot.Count;
+
+        item = a.GetComponent<Items>();
+        AddItem(item);
+        item = b.GetComponent<Items>();
+        AddItem(item);
+        item = c.GetComponent<Items>();
+        AddItem(item);
+
+    }
+
+    // 아이템을 넣기위해 모든 슬롯을 검사.
+    public bool AddItem(Items item)
+    {
+        // 슬롯에 총 개수.
+        int slotCount = Allslot.Count;
+
+        // 넣기위한 아이템이 슬롯에 존재하는지 검사.
+        for (int i = 0; i < slotCount; i++)
+        {
+            // 그 슬롯의 스크립트를 가져온다.
+            Slot slot = Allslot[i].GetComponent<Slot>();
+
+            // 슬롯이 비어있으면 통과.
+            if (!slot.isSlots())
+                continue;
+
+            // 슬롯에 존재하는 아이템의 타입과 넣을려는 아이템의 타입이 같고.
+            // 슬롯에 존재하는 아이템의 겹칠수 있는 최대치가 넘지않았을 때. (true일 때)
+            if (slot.ItemReturn().type == item.type && slot.ItemMax(item))
+            {
+                // 슬롯에 아이템을 넣는다.
+                slot.AddItem(item);
+                return true;
+            }
+        }
+
+        // 빈 슬롯에 아이템을 넣기위한 검사.
+        for (int i = 0; i < slotCount; i++)
+        {
+            Slot slot = Allslot[i].GetComponent<Slot>();
+
+            // 슬롯이 비어있지 않으면 통과
+            if (slot.isSlots())
+                continue;
+
+            slot.AddItem(item);
+            return true;
+        }
+
+        // 위에 조건에 해당되는 것이 없을 때 아이템을 먹지 못함.
+        return false;
+    }
+}
---
+++ Test Project/Assets/Scripts/Items.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    public enum TYPE { Weapon, Armor }
+
+    public TYPE type;           // 아이템의 타입.
+    public Sprite DefaultImg;   // 기본 이미지.
+    public int MaxCount;        // 겹칠수 있는 최대 숫자.  
+
+    // 인벤토리에 접근하기 위한 변수.
+    private Inventory Iv;
+
+    void Awake()
+    {
+        // 태그명이 "Inventory"인 객체의 GameObject를 반환한다.
+        // 반환된 객체가 가지고 있는 스크립트를 GetComponent를 통해 가져온다.
+        Iv = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+
+    }
+
+    void AddItem()
+    {
+        // 아이템 획득에 실패할 경우.
+        if (!Iv.AddItem(this))
+            Debug.Log("아이템이 가득 찼습니다.");
+        else // 아이템 획득에 성공할 경우.
+            gameObject.SetActive(false); // 아이템을 비활성화 시켜준다.
+    }
+
+    // 충돌체크
+    void OnTriggerEnter(Collider _col)
+    {
+        // 플레이어와 충돌하면.
+        if (_col.gameObject.layer == 10)
+            AddItem();
+    }
+}
